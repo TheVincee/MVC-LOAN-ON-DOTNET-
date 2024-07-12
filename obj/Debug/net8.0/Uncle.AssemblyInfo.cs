4cd01042473818701a46717b245f96b3dc7a4f2c
OliviaBiscuit <OliviaBiscuit@gmail.com>
2024-07-12 15:22:48 +0800

    added scaffold and Db

--- obj/Debug/net8.0/Uncle.AssemblyInfo.cs
+++ obj/Debug/net8.0/Uncle.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Uncle")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cac7f555bddedbba695ce12ad870c68a552f2833")]
 [assembly: System.Reflection.AssemblyProductAttribute("Uncle")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Uncle")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
